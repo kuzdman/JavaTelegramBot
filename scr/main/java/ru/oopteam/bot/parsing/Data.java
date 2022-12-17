1e718745a46e0bfa367bf78fc046d6390d2dd2ac
Egor Kiselev <Kiselev.Egor@urfu.me>
2022-12-17 20:59:07 +0500

    added parsing

---
+++ scr/main/java/ru/oopteam/bot/parsing/Data.java
@@ -0,0 +1,2 @@
+package ru.oopteam.bot.parsing;
+
---
+++ scr/main/java/ru/oopteam/bot/parsing/GettingCurrency.java
@@ -0,0 +1,40 @@
+package ru.oopteam.bot.parsing;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class GettingCurrency {
+    public static void main(String[] args)  {
+
+        try {
+
+            var doc = Jsoup.connect("https://www.binance.com/ru/markets").get();
+            var titleElements = doc.getElementsByClass("css-ovtrou");
+            var titleElements2 = doc.getElementsByClass("css-1x8dg53");
+
+            List<String> CryptoName = titleElements2.stream()
+                    .map(Element::text)
+                    .collect(Collectors.toList());
+            //Adding Crypto Names to list
+            List<String> Currency = titleElements.stream()
+                    .map(Element::text)
+                    .collect(Collectors.toList());
+            //Adding Crypto Currency to list
+
+            HashMap<String, String> currencyDictionary = new HashMap<>();
+            for (int i = 0; i < CryptoName.size();i++){
+                currencyDictionary.put(CryptoName.get(i), Currency.get(i));
+
+            }
+            System.out.println(currencyDictionary);
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
